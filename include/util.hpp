07ebe152b74ccf7bd200665dbbf90ee4b1673b60
rodamber <rodrigoambernardo@gmail.com>
2016-10-27 16:33:49 +0100

    Commented combinations function

--- include/util.hpp
+++ include/util.hpp
@@ -5,26 +5,26 @@
 #include <vector>
 
 // From Rosetta code (slightly modification)
-std::vector<std::vector<int>> combinations(int N, int K) {
-  std::string bitmask(K, 1); // K leading 1's
-  bitmask.resize(N, 0); // N-K trailing 0's
+// std::vector<std::vector<int>> combinations(int N, int K) {
+//   std::string bitmask(K, 1); // K leading 1's
+//   bitmask.resize(N, 0); // N-K trailing 0's
 
-  std::vector<std::vector<int>> all_combinations;
+//   std::vector<std::vector<int>> all_combinations;
 
-  // print integers and permute bitmask
-  do {
-    std::vector<int> combination;
+//   // print integers and permute bitmask
+//   do {
+//     std::vector<int> combination;
 
-    for (int i = 0; i < N; ++i) { // [0..N-1] integers
-      if (bitmask[i]) {
-        combination.push_back(i);
-      }
-    }
-    all_combinations.push_back(combination);
-  } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
+//     for (int i = 0; i < N; ++i) { // [0..N-1] integers
+//       if (bitmask[i]) {
+//         combination.push_back(i);
+//       }
+//     }
+//     all_combinations.push_back(combination);
+//   } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
 
-  return all_combinations;
-}
+//   return all_combinations;
+// }
 
 // Generator functor. Lazily generates all the possible k-combinations of the
 // numbers from 0 to N - 1.
